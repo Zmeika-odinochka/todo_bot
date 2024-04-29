286807829416a35c72e70b889388b72b3bbf1d1e
Zmeika-odinochka <kashchenkovfilipp@gmail.com>
2024-04-29 14:59:01 +0500

    v0.02

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import random
+
 HELP = """
 help - напечатать справку о программке.
 add - добавить задачу в список (название задачи уточняем у пользователя).
 show - напечатать все добавленные задачи.
 exit - Выход из программы.
+random - добавить случайную задачу на сегодня.
 """
+
+RANDOM_TASK = "Учиться программированию."
+RANDOM_TASKS = [
+    "Начать писать код", "Изучать теорию", "Проходить практикум",
+    "Смотреть видосики", "Отдыхать", "Дзен питона"
+]
+
 print(HELP)
 
 tasks = {
 
 }
+
+
+def add_todo(date, task):
+    # Проверка на существование даты в словаре.
+    if date in tasks:
+        tasks[date].append(task)
+    else:
+        tasks[date] = []
+        tasks[date].append(task)
+    print(f"Задача, '{task}', добавлена на дату, {date}")
+
 
 print("Здравствуйте.")
 
@@ -20,13 +41,8 @@ while True:
     elif command == "add":
         date = input("Введите дату для добавления задачи: ")
         task = input("Введите название задачи: ")
-        # Проверка на существование даты в словаре.
-        if date in tasks:
-            tasks[date].append(task)
-        else:
-            tasks[date] = []
-            tasks[date].append(task)
-        print(f"Задача, '{task}', добавлена на дату, {date}")
+        add_todo(date, task)
+
     elif command == "show":
         date = input("Введите дату для отображение задачи: ")
         if date in tasks:
@@ -34,6 +50,11 @@ while True:
                 print(f"- {task}")
         else:
             print("В ваших задачах такой даты нет.")
+
+    elif command == "random":
+        task = random.choice(RANDOM_TASKS)
+        add_todo("Сегодня", task)
+
     elif command == "exit":
         print("До свидания!")
         break
